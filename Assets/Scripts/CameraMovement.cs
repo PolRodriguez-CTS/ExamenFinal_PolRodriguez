211b38084182067561e34c11ba2acc2b0712311e
PolRodriguez-CTS <prodriguez@ctsformacion.com>
2025-05-28 10:19:32 +0200

    Camara

--- Assets/Scripts/CameraMovement.cs
+++ Assets/Scripts/CameraMovement.cs
@@ -5,29 +5,30 @@ using UnityEngine;
 public class CameraMovement : MonoBehaviour
 {
     public Transform playerTransform;
-    
     public Vector2 minPosition;
     public Vector2 maxPosition;
     public Vector3 offset;
     
     // Start is called before the first frame update
     void Awake()
-    {
-        playerTransform = GameObject.FindWithTag("Player").transform;
+    { 
     }
+
 
     // Update is called once per frame
     void FixedUpdate()
     {
-        if(playerTransform == null)
-        {
-            return;
-        }
-
         Vector3 cameraPosition = playerTransform.position + offset;
+
         float clampX = Mathf.Clamp(cameraPosition.x, minPosition.x, maxPosition.x);
         float clampY = Mathf.Clamp(cameraPosition.y, minPosition.y, maxPosition.y);
 
         Vector3 clampedCameraPosition = new Vector3(clampX, clampY, cameraPosition.z);
+        gameObject.transform.position = clampedCameraPosition;
+        
+        if(playerTransform == null)
+        {
+            return;
+        }
     }
 }
